fa7689097822e6e129dbc67904799fdcf8f135a7
TrySquadDF <sjpdkedkdjkdkdjdmddd@gmail.com>
2023-03-13 17:38:53 +0300

    Global: add new icon

--- api/language.ts
+++ api/language.ts
@@ -53,6 +53,8 @@ export default async (req: Request, res: Response) => {
         return resultValue;
       }, {});
 
+    console.log(languageStatistics);
+
     return res.send(card(Object.keys(languageStatistics)));
   } catch (e) {
     res.setHeader("Cache-Control", `no-cache, no-store, must-revalidate`);
